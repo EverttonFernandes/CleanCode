629a7a7f0d68aac385cc985be16f1f61b1cd5772
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2020-09-26 18:06:59 -0300

    Cria classe responsavel para fazer tratamento de exceções durate a validação dos registros que constam no relatorio de pessoas

---
+++ src/main/java/cc/code/exception/ValidadorRegistrosException.java
@@ -0,0 +1,8 @@
+package cc.code.exception;
+
+public class ValidadorRegistrosException extends Exception {
+
+    public ValidadorRegistrosException(String mensagem) {
+        super(mensagem);
+    }
+}
