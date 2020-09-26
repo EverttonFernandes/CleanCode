de2d00f7a682b7503ee5a2fc226c99d942b5871f
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2020-09-26 20:08:15 -0300

    Inicia testes unitários JUnit5

---
+++ src/test/java/ValidadorRegistros/ValidadorRegistrosTeste.java
@@ -0,0 +1,17 @@
+package ValidadorRegistros;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class ValidadorRegistrosTeste {
+
+    
+}
