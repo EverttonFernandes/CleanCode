45745799135a0611cbe6900b78fd5da15b3834bf
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2020-09-26 18:17:30 -0300

    Verifica se telefone celular da pessoa é inválido

--- src/main/java/cc/code/validador/ValidadorRegistros.java
+++ src/main/java/cc/code/validador/ValidadorRegistros.java
@@ -1,21 +1,33 @@
 package cc.code.validador;
 
 import cc.code.dominio.Pessoa;
+import cc.code.exception.ValidadorRegistrosException;
 
 public class ValidadorRegistros {
 
-    public static void validarPessoa (Pessoa pessoa) {
+    public static final String MSG_NOME_INVALIDO = "Nome inválido!";
+    public static final String MSG_TELEFONE_CELULAR_INVALIDO = "Telefone celular inválido!";
+    public static final String MSG_CPF_INVALIDO = "CPF inválido!";
 
+    public static void validarPessoa(Pessoa pessoa) throws ValidadorRegistrosException {
+        
+        validarNome(pessoa.getNome());
         validarCpf(pessoa.getCpf());
         validarTelefoneCelular(pessoa.getTelefoneCelular());
 
     }
 
-    private static void validarCpf (String cpf) {
+    private static void validarNome(String name) {
+    }
+
+    private static void validarCpf(String cpf) throws ValidadorRegistrosException{
 
     }
 
-    private static void validarTelefoneCelular(String telefone) {
-
+    private static void validarTelefoneCelular(String telefoneCelular) throws ValidadorRegistrosException {
+        if(telefoneCelular.length() < 10){
+            System.err.println(MSG_TELEFONE_CELULAR_INVALIDO);
+            throw new ValidadorRegistrosException(MSG_TELEFONE_CELULAR_INVALIDO);
+        }
     }
 }
